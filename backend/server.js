4af626d357119c71f556e4a03920779866299d51
Raghunandan Shah <Raghunandanshah908014@gmail.com>
2026-04-18 16:35:31 +0530

    change in cors

--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,8 @@ const app = express();
 
 // CORS Configuration for Production & Development
 const corsOptions = {
-  origin: process.env.CLIENT_URL || "https://lms-project-odk6.onrender.com",
-  credentials: true,
+    origin: process.env.CLIENT_URL || ["https://lms-project-odk6.onrender.com", "http://localhost:3000","https://lms-complete-project.onrender.com" ],
+    credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
